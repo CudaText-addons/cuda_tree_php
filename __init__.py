5001241427e1b1ec9b5c1d5e78fc5158a3700f45
Alexey-t <support@uvviewsoft.com>
2018-05-17 11:04:45 +0300

    chk icon of 'trait'

--- __init__.py
+++ __init__.py
@@ -3,22 +3,28 @@ from . import pyphptree
 def kind_to_icon(s):
     if s=='class': return 1
     if s=='function': return 5
+    if s=='trait': return 2
     return 0
 
 def get_headers(filename, lines):
     r = []
-    for v in pyphptree.get_headers(filename, lines):
+    items = list(pyphptree.get_headers(filename, lines))
+
+    # todo
+    # post processing: merge nodes of namespaces with same names
+
+    for v in items:
         name = v['name'] or '??'
         kind = kind_to_icon(v['kind'])
-        
+
         r += [ (
-            v['line'], 
-            v['level']+1, 
-            name, 
+            v['line'],
+            v['level']+1,
+            name,
             kind,
             ) ]
     return r
-        
+
 
 helper = {
   'PHP': get_headers,
