876deb121ad948100002acec3847cecee2d8548f
Alexey-t <support@uvviewsoft.com>
2019-02-12 23:00:29 +0300

    adapte to new Cud

--- __init__.py
+++ __init__.py
@@ -1,14 +1,19 @@
 from . import pyphptree
 
 def kind_to_icon(s):
+
     if s=='class': return 1
     if s=='function': return 5
     if s=='trait': return 2
     return 0
 
 def get_headers(filename, lines):
+    '''
+    gets list of tuples in format:
+    ( (x1,y1,x2,y2), level, title, icon)
+    '''
     r = []
-    items = list(pyphptree.get_headers(filename, lines))
+    items = pyphptree.get_headers(filename, lines)
 
     # todo
     # post processing: merge nodes of namespaces with same names
@@ -16,9 +21,11 @@ def get_headers(filename, lines):
     for v in items:
         name = v['name'] or '??'
         kind = kind_to_icon(v['kind'])
+        line = v['line']
+        col = v['col']
 
-        r += [ (
-            v['line'],
+        r += [ ( 
+            (col, line, col+1, line),
             v['level']+1,
             name,
             kind,
